12eb12e3b00af18b54240e285f9319c7f000b608
Sharaar <walidsharar@gmail.com>
2021-06-19 11:31:23 +0200

    Project Completed

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
+import turtle as turtle_mod
+import random
 
-import colorgram
+turtle_mod.colormode(255)
+my_turtle = turtle_mod.Turtle()
+my_turtle.speed("fastest")
+my_turtle.penup()
+my_turtle.hideturtle()
+color_list = [(202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40), (140, 30, 19), (133, 163, 185), (198, 91, 71), (46, 122, 86), (72, 43, 35), (145, 178, 148), (13, 99, 71), (233, 175, 164), (161, 142, 158), (105, 74, 77), (55, 46, 50), (183, 205, 171), (36, 60, 74), (18, 86, 90), (81, 148, 129), (148, 17, 20), (14, 70, 64), (30, 68, 100), (107, 127, 153), (174, 94, 97), (176, 192, 209)]
+my_turtle.setheading(225)
+my_turtle.forward(300)
+my_turtle.setheading(0)
+dots = 100
 
-rgb_colors = []
-colors = colorgram.extract('image.jpg', 30)
-for color in colors:
-    r=color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color =(r,g,b)
-    rgb_colors.append(new_color)
+for dot_count in range(1, dots + 1):
+    my_turtle.dot(20, random.choice(color_list))
+    my_turtle.forward(50)
 
-print(rgb_colors)
+    if dot_count % 10 == 0:
+        my_turtle.setheading(90)
+        my_turtle.forward(50)
+        my_turtle.setheading(180)
+        my_turtle.forward(500)
+        my_turtle.setheading(0)
+
+
+
+
+
+
+
+
+
+screen = turtle_mod.Screen()
+screen.exitonclick()
